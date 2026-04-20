e7d37049c309fb901d2b2ed50233a393798fd76b
Goooosi <166347594+Goooosi@users.noreply.github.com>
2026-04-19 21:51:37 -0400

    Added router in backend

---
+++ backend/chat-app/src/main/java/com/goooosi/chat_app/controller/RouterController.java
@@ -0,0 +1,12 @@
+package com.goooosi.chat_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RouterController {
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
